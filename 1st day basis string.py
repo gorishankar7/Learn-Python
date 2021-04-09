35a42896401e1d77e37b3a84666a4d699ddbebe2
Gorishankar <47425942+gorishankar7@users.noreply.github.com>
2021-04-09 15:38:35 +0530

    Learn Python Basis
    Let's Start python from starting

---
+++ 1st day basis string.py
@@ -0,0 +1,34 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 9*9
+81
+>>> 9**5
+59049
+>>> 9/5
+1.8
+>>> 9//5
+1
+>>> 9%5
+4
+>>> 9%%5
+SyntaxError: invalid syntax
+>>> a='gorishankar'
+>>> a[2:5]
+'ris'
+>>> a[4:9]
+'shank'
+>>> a[-6:-2]
+'hank'
+>>> a[-6:-2:-1]
+''
+>>> a[-2:-6:-1]
+'akna'
+>>> a[2]
+'r'
+>>> a[2:2]
+''
+>>> a[2::2]
+'rsakr'
+>>> a[1:1]
+''
+>>> 
